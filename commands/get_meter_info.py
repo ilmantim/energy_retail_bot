79f09b31c6e3025b8cdb626710b0954e7f128ff1
ilmantim <ilmantimrod@gmail.com>
2023-12-27 11:41:26 +0100

    Update get_meter_info.py

--- commands/get_meter_info.py
+++ commands/get_meter_info.py
@@ -93,13 +93,25 @@ def get_meter_info(update: Update, context: CallbackContext) -> int:
                 else:
                     context.bot.send_message(
                         chat_id=update.effective_chat.id,
-                        text="Не удалось найти счет."
+                        text="Проверьте правильность введения номера лицевого счета.\n"
+                         "Возможно, по данному адресу приборы учёта отсутствуют или закончился срок поверки.\n"
+                         "Для уточнения информации обратитесь к специалисту контакт-центра"
+                    )
+                    update.message.reply_text(
+                        "Введите лицевой счёт",
+                        reply_markup=submit_readnigs_and_get_meter_keyboard()
                     )
                     return METER_INFO
             else:
                 context.bot.send_message(
                     chat_id=update.effective_chat.id,
-                    text="Не удалось найти счет."
+                    text="Проверьте правильность введения номера лицевого счета.\n"
+                         "Возможно, по данному адресу приборы учёта отсутствуют или закончился срок поверки.\n"
+                         "Для уточнения информации обратитесь к специалисту контакт-центра"
+                )
+                update.message.reply_text(
+                    "Введите лицевой счёт",
+                    reply_markup=submit_readnigs_and_get_meter_keyboard()
                 )
                 return METER_INFO
             
