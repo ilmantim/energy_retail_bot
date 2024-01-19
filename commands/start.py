f5edb1e642377287ad56bcc30a5a6edb6bec2b6f
ilmantim <ilmantimrod@gmail.com>
2024-01-19 15:37:18 +0100

    Refactor handle_start function

--- commands/start.py
+++ commands/start.py
@@ -3,7 +3,7 @@ import logging
 from telegram import Update
 from telegram.ext import CallbackContext
 from retail.models import Customer  
-from keyboard import main_menu_keyboard  
+from keyboard import main_menu_keyboard
 from messages import HELLO
 
 
@@ -13,32 +13,46 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-MAIN_MENU, SUBMIT_READINGS, INPUT_READINGS, YES_OR_NO_ADDRESS, METER_INFO,\
-    CONTACT_INFO, CREATE_FAVORITE_BILL, REMOVE_FAVORITE_BILLS = range(8)
+MAIN_MENU = 0
 
 
-def handle_start(update: Update, context: CallbackContext) -> int:
-    logger.info("кнопка_старт")
+def get_or_create_customer(update: Update, context: CallbackContext) -> tuple:
+    user, is_found = Customer.objects.get_or_create(
+        chat_id=update.effective_chat.id
+    )
+    context.user_data['chat_id'] = user.chat_id
+    return user, is_found
 
-    if context.user_data.get('has_started', True):
-        user, is_found = Customer.objects.get_or_create(
-            chat_id=update.effective_chat.id
+
+def check_user_bills(user) -> bool:
+    if user.favorites.count() > 0:
+        return True
+    return False
+
+
+def send_start_message(update: Update, has_bills: bool = False):
+    if has_bills:
+        update.message.reply_text(
+            "Выберите раздел",
+            reply_markup=main_menu_keyboard(True)
         )
-        context.user_data['chat_id'] = user.chat_id
-        if user.favorites.count() > 0:
-            context.user_data['bills_count'] = user.favorites.count()
-            bills = True
-        else:
-            bills = False
-        update.message.reply_text("Выберите раздел",
-                                  reply_markup=main_menu_keyboard(bills))
     else:
-
-        context.user_data['has_started'] = True
-        update.message.reply_text(HELLO)
-
         update.message.reply_text(
             "Выберите раздел",
             reply_markup=main_menu_keyboard()
         )
+
+
+def handle_start(update: Update, context: CallbackContext) -> int:
+    logger.info("start.py")
+
+    if context.user_data.get('has_started', True):
+        user, _ = get_or_create_customer(update, context)
+        has_bills = check_user_bills(user)
+        send_start_message(update, has_bills)
+    else:
+        context.user_data['has_started'] = True
+        update.message.reply_text(HELLO)
+        send_start_message(update)
+
     return MAIN_MENU
